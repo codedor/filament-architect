9f0316d09faa493a482ceae04f0240c311b04432
jyrkidn <jyrkidn@users.noreply.github.com>
2023-07-06 10:05:58 +0000

    Dusting

--- src/Filament/Architect/VideoBlock.php
+++ src/Filament/Architect/VideoBlock.php
@@ -3,11 +3,6 @@
 namespace Codedor\FilamentArchitect\Filament\Architect;
 
 use Awcodes\FilamentOembed\Forms\Components\OEmbed;
-use Filament\Forms\Components\Checkbox;
-use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\Tabs;
-use Filament\Forms\Components\Tabs\Tab;
-use Filament\Forms\Components\TextInput;
 
 class VideoBlock extends BaseBlock
 {
