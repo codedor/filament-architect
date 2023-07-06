1ee34529691f4be79e8f617be05659e03d6010f6
Jyrki De Neve <jyrki.deneve@gmail.com>
2023-07-06 13:05:21 +0300

    Finalize Video block

--- src/Filament/Architect/VideoBlock.php
+++ src/Filament/Architect/VideoBlock.php
@@ -2,6 +2,7 @@
 
 namespace Codedor\FilamentArchitect\Filament\Architect;
 
+use Awcodes\FilamentOembed\Forms\Components\OEmbed;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Tabs;
@@ -13,35 +14,7 @@ class VideoBlock extends BaseBlock
     public function schema(): array
     {
         return [
-            Tabs::make('video')
-                ->tabs([
-                    Tab::make('Video link')
-                        ->schema([
-                            Radio::make('type')
-                                ->options([
-                                    'youtube' => 'Youtube',
-                                    'vimeo' => 'Vimeo',
-                                ]),
-                            TextInput::make('url')
-                                ->placeholder('Video ID or URL'),
-                        ]),
-                    Tab::make('Player Settings')
-                        ->schema([
-                            Checkbox::make('autoplay'),
-                            Checkbox::make('muted'),
-                            Checkbox::make('loop'),
-                            Checkbox::make('fullscreen'),
-                            Checkbox::make('show_info'),
-                            Checkbox::make('modest_branding'),
-                        ]),
-                    Tab::make('Layout')
-                        ->schema([
-                            TextInput::make('width')
-                                ->numeric()
-                                ->suffix('%')
-                                ->helperText('Width of the video iframe in %'),
-                        ]),
-                ]),
+            OEmbed::make('video'),
         ];
     }
 }
