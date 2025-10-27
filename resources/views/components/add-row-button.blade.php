cfd828cc7eb813b59c17cd5a57a77753539d44b1
Thomva <thomvadev@gmail.com>
2025-10-27 16:20:29 +0000

    Make inputs more responsive, refs #FIL001-174

--- resources/views/components/add-row-button.blade.php
+++ resources/views/components/add-row-button.blade.php
@@ -5,14 +5,17 @@
     'shown' => false,
     'label' => 'Insert block',
     'aligned' => 'center',
+    'class' => null,
 ])
 
 <div @class([
-    'z-10 mx-auto w-4/5 h-0' => ($aligned === 'center'),
+    'z-10 mx-auto w-4/5' => ($aligned === 'center'),
+    $class,
 ])>
     <div @class([
-        'flex gap-4' => ($aligned === 'left'),
-        'flex justify-center z-10 w-full h-8 -mt-4 gap-4' => ($aligned === 'center'),
+        'flex flex-wrap',
+        'gap-4' => ($aligned === 'left'),
+        'justify-center z-10 w-full -mt-4 gap-4' => ($aligned === 'center'),
         'opacity-0 hover:opacity-100 transition-all' => ! $shown,
     ])>
         <x-filament-architect::icon-button
